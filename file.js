908f882c68bc2714e09a667fa9e324b8ac50123a
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-07-08 00:37:57 +0100

    Create file.js

---
+++ file.js
@@ -0,0 +1,10 @@
+function titleCase(str) {
+  for (let i = 0; i < str.length; i++) {
+    let regex = //; //this finds the right letter
+    let replacement = //; //this states what to replace with
+    let capitalised = str.replace(regex, replacement);
+  }
+  return capitalised;
+}
+
+titleCase("I'm a little tea pot");
